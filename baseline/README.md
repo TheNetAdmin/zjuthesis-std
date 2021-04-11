23262fece679d82cd7ea4eb9aea4c8caf7f3baaf
TheNetAdmin <zxwang42@gmail.com>
2021-04-11 01:29:40 -0700

    Update single pdf

--- baseline/README.md
+++ baseline/README.md
@@ -4,16 +4,18 @@
 
 ```json
 {
-    "name": "xxx.pdf",
-    "zjuthesis": {
-        "commit": "git-id",
-        "arg-xxx": "value-xxx"
-    },
-    "texlive": {
-        "suite": "native|docker@xxx/xxx:xxx",
-        "latexmk": "latexmk-version",
-        "xelatex": "xelatex-version"
-    },
-    "time": "timestamp"
+    "xxx.pdf": 
+    {
+        "zjuthesis": {
+            "commit": "git-id",
+            "arg-xxx": "value-xxx"
+        },
+        "texlive": {
+            "suite": "native|docker@xxx/xxx:xxx",
+            "latexmk": "latexmk-version",
+            "xelatex": "xelatex-version"
+        },
+        "time": "timestamp"
+    }
 }
 ```
--- baseline/update.py
+++ baseline/update.py
@@ -10,11 +10,8 @@ import click
 
 
 @click.group()
-@click.pass_context
-def update(ctx):
-    ctx.ensure_object(dict)
-    with open('baseline.json', 'r') as f:
-        ctx.obj['baseline'] = json.load(f)
+def update():
+    pass
 
 
 @update.command()
@@ -23,30 +20,39 @@ def update(ctx):
 @click.option('-t', '--thesis_type', required=True, type=click.Choice(['thesis', 'design']))
 @click.option('-p', '--period', required=True, type=click.Choice(['proposal', 'final']))
 @click.option('-b', '--blind', is_flag=True)
-@click.option('-l', '--grad_level', required=True, default='master', type=click.Choice(['master', 'doctor']))
-@click.pass_context
-def single(ctx, major, degree, thesis_type, period, blind, grad_level):
+@click.option('-g', '--grad_level', required=True, default='master', type=click.Choice(['master', 'doctor']))
+@click.option('-l', '--language', required=True, default='chinese', type=click.Choice(['chinese', 'english']))
+def single(major, degree, thesis_type, period, blind, grad_level, language):
     '''Update single baseline pdf file'''
-    update_single(ctx, major, degree, thesis_type, period, blind, grad_level)
+    update_single(major, degree, thesis_type,
+                  period, blind, grad_level, language)
 
 
 @update.command()
-@click.pass_context
-def preset(ctx):
+def preset():
     '''Update preset baseline pdf files'''
     pass
 
 
-def update_single(ctx, major, degree, thesis_type, period, blind, grad_level):
+def update_single(major, degree, thesis_type, period, blind, grad_level, language):
     # Single pdf config
-    curr_cfg = dict()
+    with open('baseline.json', 'r') as f:
+        cfg = json.load(f)
 
     # Output filename
-    filename = f'zjuthesis-{degree}-{grad_level}-{grad_level}-{major}-{thesis_type}-{period}'
+    filename = f'zjuthesis-{degree}'
+    if degree == 'graduate':
+        filename += f'-{grad_level}'
+    filename += f'-{major}-{thesis_type}-{period}'
     if blind:
         filename += f'-blind'
+    if language != 'chinese':
+        filename += f'-{language}'
     filename += f'.pdf'
-    curr_cfg['filename'] = filename
+
+    # Prev config
+    curr_cfg = dict()
+    prev_cfg = cfg[filename] if filename in cfg.keys() else None
 
     # Texlive
     tl = texlive()
@@ -56,21 +62,33 @@ def update_single(ctx, major, degree, thesis_type, period, blind, grad_level):
     curr_time = datetime.utcnow()
     curr_cfg['time'] = curr_time.isoformat()
     sub_dir = f'zjuthesis-{curr_time.isoformat()}'
-    with chmkdir(Path('.') / 'out'):
-        run(['git', 'clone', '--depth=1', 'git@github.com:thenetadmin/zjuthesis', sub_dir])
-    with chdir(Path('.') / 'out' / sub_dir):
+    baseline_dir = Path('.').resolve()
+    with chmkdir(baseline_dir / 'out'):
+        run(['git', 'clone', '--depth=1',
+            'git@github.com:thenetadmin/zjuthesis', sub_dir], capture_output=False)
+    with chdir(baseline_dir / 'out' / sub_dir):
         curr_cfg['zjuthesis'] = dict()
         curr_cfg['zjuthesis']['commit_id'] = run(
             ['git', 'rev-parse', '--short', 'HEAD'])
+        if prev_cfg and prev_cfg['zjuthesis']['commit_id'] == curr_cfg['zjuthesis']['commit_id']:
+            print(
+                f'Commit ID {curr_cfg["zjuthesis"]["commit_id"]} matches previous record, skip {filename}')
+            return
         curr_cfg['zjuthesis']['major'] = major
         curr_cfg['zjuthesis']['degree'] = degree
         curr_cfg['zjuthesis']['thesis_type'] = thesis_type
         curr_cfg['zjuthesis']['period'] = period
         curr_cfg['zjuthesis']['blind'] = blind
         curr_cfg['zjuthesis']['grad_level'] = grad_level
-    
+        curr_cfg['zjuthesis']['language'] = language
+        run(['bash', 'script/ci/setup.sh'], capture_output=False)
+        tl.compile(Path('.'), curr_cfg['zjuthesis'])
+        new_pdf = Path('.') / 'dist' / filename
+        new_pdf.rename(baseline_dir/filename)
 
-    print(curr_cfg)
+    cfg[filename] = curr_cfg
+    with open('baseline.json', 'w') as f:
+        json.dump(cfg, f, indent=4)
 
 
 @contextlib.contextmanager
@@ -99,8 +117,11 @@ def chdir(path):
 
 def run(cmd: list, capture_output=True):
     try:
-        return subprocess.run(cmd, capture_output=capture_output,
-                              check=True).stdout.decode('utf-8').strip()
+        if capture_output:
+            return subprocess.run(cmd, capture_output=True,
+                                  check=True).stdout.decode('utf-8').strip()
+        else:
+            subprocess.run(cmd, check=True)
     except subprocess.CalledProcessError:
         subprocess.run(cmd, check=True)
 
@@ -120,8 +141,21 @@ class texlive:
         cfg['xelatex'] = res.split('\n')[0]
         return cfg
 
-    def compile(self, dir):
-        pass
+    def compile(self, path: Path, cfg: dict):
+        cmd = []
+        cmd += ['docker', 'run']
+        cmd += ['--user', '1000:1000']
+        cmd += ['--mount', f'src={path.resolve()},target=/zjuthesis,type=bind']
+        cmd += ['--rm', 'adnrv/texlive:full']
+        cmd += ['bash', '/zjuthesis/script/ci/github-action/build_single.sh']
+        cmd += [cfg['degree']]
+        cmd += [cfg['major']]
+        cmd += [cfg['thesis_type']]
+        cmd += [cfg['period']]
+        cmd += ['true' if cfg['blind'] else 'false']
+        cmd += [cfg['grad_level']]
+        cmd += [cfg['language']]
+        run(cmd, capture_output=False)
 
 
 if __name__ == '__main__':
